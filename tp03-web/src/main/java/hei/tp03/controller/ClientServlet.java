76e1877259465cd584f6478a7dd1662f793aa0aa
PiSurQuatre <david.picart@outlook.fr>
2017-01-21 01:51:22 +0100

    Affichage des informations de la BDD sur la page grâce à un PrintWriter (Utilisation des nouveaux services)

--- tp03-web/src/main/java/hei/tp03/controller/ClientServlet.java
+++ tp03-web/src/main/java/hei/tp03/controller/ClientServlet.java
@@ -1,9 +1,16 @@
 package hei.tp03.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.BaseJsonNode;
 import hei.tp03.entity.Client;
+import hei.tp03.entity.Commande;
+import hei.tp03.entity.Produit;
 import hei.tp03.service.ClientService;
+import hei.tp03.service.CommandeService;
+import hei.tp03.service.ProduitService;
 import javafx.print.Printer;
+import org.hibernate.Hibernate;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,12 +21,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(name="ClientServlet", urlPatterns = {"/*"})
 public class ClientServlet  extends HttpServlet {
 
     private ClientService clientService;
+
+    private CommandeService commandeService;
+
+    private ProduitService produitService;
 
     private ConfigurableApplicationContext context;
 
@@ -28,15 +41,45 @@ public class ClientServlet  extends HttpServlet {
     {
         context = new AnnotationConfigApplicationContext("hei.tp03.config");
         clientService = (ClientService) context.getBean(ClientService.class);
+
+        commandeService = (CommandeService) context.getBean(CommandeService.class);
+
+        produitService = (ProduitService) context.getBean(ProduitService.class);
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         List<Client> listeClients = clientService.findAll();
+
+        //J'abandonne l'affichage sous forme de JSON,
+        // je n'arrive pas à charger les Listes de commandes et de produits
+        // sans passer par Fetch = FetchType.EAGER
+        // J'ai essayé avec if(!Hibernate.isInitialized())
+        //                      Hibernate.initialize();
+        // Sans succès. J'ai une brouette de questions pour le prochain cours.
+
+        /*
         resp.setHeader("Content-Type","application/json");
         ObjectMapper mapper = new ObjectMapper();
         String jsonAAfficher = mapper.writeValueAsString(listeClients);
         resp.getWriter().println(jsonAAfficher);
+        */
+
+        PrintWriter out = resp.getWriter();
+        for (Client client:listeClients) {
+            out.println("Client: "+client.getId()+" - "+client.getNom()+" "+client.getPrenom());
+            List<Commande> listeCommandes = commandeService.findByClientId(client.getId());
+            for (Commande commande:listeCommandes) {
+                out.println("\n\tCommande: "+commande.getId()+" - Validée "+commande.getValidee());
+                List<Produit> listeProduits = produitService.findByCommandeId(commande.getId());
+                for (Produit produit: listeProduits) {
+                    out.println("\n\t\tProduit: "+produit.getId()+" - "+produit.getNom());
+                }
+            }
+            out.println("\n");
+        }
+
+
     }
 
     @Override
